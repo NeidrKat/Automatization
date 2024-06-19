ce85d43c953643e17f800e88beb260f28cc5e5c1
NaidrKate <buldakova1394@gmail.com>
2024-06-19 14:17:41 +0300

    HW3

---
+++ Lesson3/address.py
@@ -0,0 +1,7 @@
+class Address:
+    def __init__(self, index, city, street, home, flat):
+        self.i = index
+        self.c = city
+        self.str = street
+        self.h = home
+        self.f = flat
---
+++ Lesson3/mailing.py
@@ -0,0 +1,7 @@
+
+class Mailing:
+    def __init__(self, to_address, from_address, cost, track):
+        self.t = to_address
+        self.f = from_address
+        self.c = cost
+        self.track = track
