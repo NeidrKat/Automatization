254ae656d34d0710a2239705352ace1fe2967986
NaidrKate <buldakova1394@gmail.com>
2024-08-22 23:10:59 +0300

    HW9

--- Lesson9/CompanyAPI.py
+++ Lesson9/CompanyAPI.py
@@ -1,38 +1,15 @@
 import requests
 
 
-class ApiEmployee:
-
-    def __init__(self, url) -> None:
+class CompanyApi:
+    def __init__(self, url):
         self.url = url
 
-# Авторизация
-    def auth2(self, login="leonardo", password="leads"):
-        body = {
-          "username": login,
-          "password": password
+    def get_token(self, user="flora", password="nature-fairy"):
+        creds = {
+            "username": user,
+            "password": password
         }
-        response = requests.post(self.url + '/auth/login', json=body)
-        return response.json()["userToken"]
-
-# Получить список сотрудников компании
-    def get_list_employee2(self, params=None):
-        response = requests.get(self.url + '/employee' + params)
-        return response
-
-# Добавить нового сотрудника
-    def add_new_employee2(self, body):
-        headers = {'x-client-token': self.auth2()}
-        response = requests.post(self.url + '/employee/', headers=headers, json=body)
-        return response
-
-# Получить сотрудника по id
-    def get_new_employee2(self, id):
-        response = requests.get(self.url + '/employee/' + str(id))
-        return response
-
-# Изменить данные о новом сотруднике
-    def change_new_employee2(self, id, new_body):
-        headers = {'x-client-token': self.auth2()}
-        response = requests.patch(self.url + '/employee/' + str(id), headers=headers, json=new_body)
-        return response
+        resp = requests.post(f'{self.url}/auth/login', json=creds)
+        token = resp.json()["userToken"]
+        return token
